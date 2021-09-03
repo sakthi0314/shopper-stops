977e3cabbe4903512893b2a546a9a1acecbe0f96
SathishVM <sathishvm37@gmail.com>
2021-09-04 01:58:25 +0530

    add missing require in validation index

--- server/src/validations/index.js
+++ server/src/validations/index.js
@@ -1,2 +1,5 @@
 module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
+module.exports.productValidation = require('./product.validation');
+module.exports.reviewValidation = require('./review.validation');
+module.exports.shippingAddressValidation = require('./shippingAddress.validation');
