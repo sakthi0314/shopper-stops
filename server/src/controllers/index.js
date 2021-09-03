36e3fa265266fda5fab4b5b14e81977f2ff5cac8
SathishVM <sathishvm37@gmail.com>
2021-09-03 23:31:06 +0530

    add shipping address controller to index

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -2,3 +2,4 @@ module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.productController = require('./product.controller');
 module.exports.reviewController = require('./review.controller');
+module.exports.shippingAddressController = require('./shippingAddress.controller');
